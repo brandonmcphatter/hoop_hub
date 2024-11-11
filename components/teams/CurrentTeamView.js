95136669372da4be028e6911ba518a75b00cf859
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-11-10 22:25:15 -0500

    refactored context to separate file

--- components/teams/CurrentTeamView.js
+++ components/teams/CurrentTeamView.js
@@ -2,58 +2,47 @@
 import {nbaTeams} from "@/teams";
 import PlayerTable from "@/components/teams/PlayerTable";
 import TeamLine from "@/components/teams/TeamLine";
-import {createContext, useContext, useState} from "react";
 import CurrentLogo from "@/components/teams/CurrentLogo";
-
-// create context
-const TeamsContext = createContext();
-
-// provider
-export function Teams() {
-    const [teamKey, setTeamKey] = useState(0);
-    const currentTeam = nbaTeams[teamKey];
-    const changeTeams = (x) => {
-        setTeamKey(x);
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-        });
-    }
-    return (
-        <TeamsContext.Provider value={{
-            teamKey,
-            setTeamKey,
-            currentTeam,
-            changeTeams,
-        }}>
-            <CurrentTeamView/>
-        </TeamsContext.Provider>
-    )
-}
+import {useTeamsContext} from "@/components/TeamsProvider";
+import {Suspense} from "react";
 
 export default function CurrentTeamView() {
-    let {teamKey, changeTeams, currentTeam} = useContext(TeamsContext);
+
+    let {teamKey, changeTeams, currentTeam} = useTeamsContext();
 
     return (
-        <div className={'px-3 pt-8'}>
-            {/*team logo*/}
-            <CurrentLogo teamKey={teamKey}/>
-            {/*player info*/}
-            <PlayerTable city={currentTeam.city} name={currentTeam.name}/>
-            {/*list of teams*/}
-            <div className={' text-4xl'}>Teams</div>
-            <hr className={'mt-4 mb-6'}/>
-            <div className={'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mb-10'}>
-                {nbaTeams.map((team, index) => {
-                    return <TeamLine city={team.city}
-                                     name={team.name}
-                                     logo={team.logo}
-                                     teamKey={teamKey}
-                                     setTeamKey={changeTeams}
-                                     teamNum={index}
-                                     key={index}/>
-                })}
+        <>
+            <div className={'flex-col w-full max-h-svh'}>
+
+                <div className={'px-3 md:px-10 pt-8  mx-auto w-full'}>
+                    {/*team logo*/}
+                    <Suspense fallback={<p>Loading Logo . . . </p>}>
+                        <CurrentLogo teamKey={teamKey}/>
+                    </Suspense>
+
+                    {/*player info*/}
+                    <Suspense fallback={<p>Loading Roster . . . </p>}>
+                        <PlayerTable city={currentTeam.city} name={currentTeam.name}/>
+                    </Suspense>
+
+                    {/*list of teams*/}
+                    <div className={' md:hidden mx-2'}>
+                        <div className={' text-4xl'}>Teams</div>
+                        <hr className={'mt-4 mb-6'}/>
+                        <div className={'grid grid-cols-1 md:grid-cols-2 gap-5 mb-10'}>
+                            {nbaTeams.map((team, index) => {
+                                return <TeamLine city={team.city}
+                                                 name={team.name}
+                                                 logo={team.logo}
+                                                 teamKey={teamKey}
+                                                 setTeamKey={changeTeams}
+                                                 teamNum={index}
+                                                 key={index}/>
+                            })}
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
+        </>
     );
 }
