9e1376ec5041f0fb8bddb7e70562a772a4af4e20
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-11-10 22:26:38 -0500

    basic footer

---
+++ components/ui/Footer.js
@@ -0,0 +1,11 @@
+export default function Footer() {
+
+    return (
+        <footer className={'bg-gray-100 '}>
+            <div className={'text-center py-2'}>
+                <div className={''} >&copy; 2024 Hoop Hub. All Rights Reserved.</div>
+
+            </div>
+        </footer>
+    )
+}
