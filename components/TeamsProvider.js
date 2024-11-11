3a74aab1e622e193c45ba44c062b0e4a8d13cd50
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-11-10 22:25:24 -0500

    created context

--- components/TeamsProvider.js
+++ components/TeamsProvider.js
@@ -2,17 +2,18 @@
 
 import {createContext, useContext, useEffect, useState} from "react";
 import {nbaTeams} from "@/teams";
-
+import {ballDontLie, API_KEY} from '@/ballDontLie';
+import {redirect} from "next/navigation";
 
 const TeamsContext = createContext();
 
 // provider
 export function TeamsContextProvider({children}) {
+    const [isLoading, setIsLoading] = useState(false);
     const [teamKey, setTeamKey] = useState(0); // index of current team
     const [players, setPlayers] = useState([]); // all players
     const currentTeam = nbaTeams[teamKey]; // team object
 
-    // changes current team and scroll to top
     const changeTeams = (x) => {
         setTeamKey(x);
         window.scrollTo({
@@ -20,25 +21,35 @@ export function TeamsContextProvider({children}) {
             behavior: 'smooth'
         });
     }
-    // fetch all players
-    const fetchPlayers = async () => {
-        try {
-            const response = await fetch('https://api.balldontlie.io/v1/teams/<1>',{
-                headers: {
-                        Authorization: ''
-                    }
-                },
-            );
-            const data = await response.json();
-            setPlayers(data.data);
-        } catch (error) {
-            console.error('Error fetching player data:', error);
-        }
-    };
 
-    useEffect(() => {
+    const goToTeam = (x)=> {
+        redirect('/teams');
+        setTeamKey(x);
+    }
+
+    // fetch all players
+
+    useEffect(()=>{
+        const fetchPlayers = async () => {
+            try {
+                console.log('loading');
+                const response = await fetch(ballDontLie+`players?team_ids[]=${teamKey+1}`,{
+                        headers: {Authorization: API_KEY}
+                    },
+                );
+                const data = await response.json();
+                setPlayers(data.data);
+                console.log('done');
+            } catch (error) {
+                console.error('Error fetching player data:', error);
+            } finally {
+
+            }
+        };
         fetchPlayers();
-    }, []);
+
+    }, [teamKey])
+    console.log(players)
 
     return (
         <TeamsContext.Provider value={{
@@ -48,6 +59,7 @@ export function TeamsContextProvider({children}) {
             setPlayers,
             currentTeam,
             changeTeams,
+            goToTeam
         }}>
             {children}
         </TeamsContext.Provider>
