2c75fb924e8fd5c610b535c3aa12dcf426e1f7e7
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-11-10 22:28:29 -0500

    team button to go to team search

--- components/home/TeamSquare.js
+++ components/home/TeamSquare.js
@@ -1,15 +1,18 @@
-import Image from "next/image";
+'use client'
 
-export default function TeamSquare({city, name, logo}) {
+import Image from "next/image";
+import {useTeamsContext} from "@/components/TeamsProvider";
+
+export default function TeamSquare({city, name, logo, teamNum}) {
+    let {goToTeam} = useTeamsContext();
     return (
-        <div>
-            {/*TeamBlock*/}
-            <div className={'w-full h-48 flex flex-col items-center justify-center '}>
-                <div>
+            <div onClick={()=>goToTeam(teamNum)} className={'w-full h-56 md:h-64 lg:h-72 flex flex-col items-center justify-between bg-gray-200 rounded-lg overflow-auto cursor-pointer shadow drop-shadow'}>
+                <div className={'h-2/3 w-full flex justify-center items-center'}>
                     <Image src={logo} alt={city +'' + name} width={100} height={100}/>
                 </div>
-                <div className={''}>{city} {name}</div>
+                <div className={'w-full h-1/3 bg-gray-500 content-center'}>
+                    <div className={'text-center text-gray-50 text-lg font-semibold'} style={{fontFamily: 'Helvetica'}}>{city} {name}</div>
+                </div>
             </div>
-        </div>
     )
 }
