eee57b42b39f106c57c7cb14cd8c21db53df8154
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-11-10 22:24:56 -0500

    updated hero section

--- components/teams/CurrentLogo.js
+++ components/teams/CurrentLogo.js
@@ -4,8 +4,10 @@ import {nbaTeams} from "@/teams";
 export default function CurrentLogo({teamKey}) {
     return (
         <div className={''}>
-            <Image className={'mx-auto'} src={nbaTeams[teamKey].logo} alt={nbaTeams[teamKey].name} width={350}
+            <Image className={'mx-auto lg:hidden'} src={nbaTeams[teamKey].logo} alt={nbaTeams[teamKey].name} width={350}
                    height={350}/>
+            <Image className={'mx-auto hidden lg:block'} src={nbaTeams[teamKey].logo} alt={nbaTeams[teamKey].name} width={500}
+                   height={500}/>
         </div>
     )
 }
