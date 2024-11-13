ec1eab6e1b1d38bf7385a024a3464a64aa649092
Brandon McPhatter <brandonmcphatter1@gmail.com>
2024-11-12 19:36:33 -0500

    moved useEffect for loading players to teams page

--- app/(teams)/teams/page.js
+++ app/(teams)/teams/page.js
@@ -1,10 +1,32 @@
 'use client';
 
 import CurrentTeamView from "@/components/teams/CurrentTeamView";
+import {useEffect} from "react";
+import {API_KEY, ballDontLie} from "@/ballDontLie";
+import {useTeamsContext} from "@/components/TeamsProvider";
 
 export default function TeamPage() {
+    let {setPlayers, activeTeam, teamKey} = useTeamsContext();
+    useEffect(()=>{
+        const fetchPlayers = async () => {
+            try {
+                console.log('loading');
+                const response = await fetch(ballDontLie+`players?team_ids[]=${teamKey+1}`,{
+                        headers: {Authorization: API_KEY}
+                    },
+                );
+                const data = await response.json();
+                setPlayers(data.data);
+                console.log('done');
+            } catch (error) {
+                console.error('Error fetching player data:', error);
+            } finally {
 
+            }
+        };
+        fetchPlayers();
 
+    }, [teamKey])
     return (
 
         <CurrentTeamView />
